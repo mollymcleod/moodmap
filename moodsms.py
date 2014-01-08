bf904cda26a94e9ff2b22df9347fa102b225e0dc
Jake Solomon <jacob@codeforamerica.org>
2014-01-07 16:41:19 -0800

    added username attrbute

--- moodsms.py
+++ moodsms.py
@@ -1,5 +1,7 @@
 import os, urllib2
 import json
+import re
+from pprint import pprint
 from twilio.rest import TwilioRestClient
 from datetime import datetime
 from babel.dates import format_datetime
@@ -30,16 +32,16 @@ def index():
   users = User.query.all()
   return render_template('index.html', users = users)
 
-@app.route('/<phone_number>')
-def calendar(phone_number):
-  u = User.query.filter_by(phone_number = phone_number).first_or_404()
+@app.route('/<username_url>')
+def calendar(username_url):
+  u = User.query.filter_by(username_url = username_url).first_or_404()
   return render_template('calendar.html', user = u)
 
 @app.route('/sms')
 def sms():
   msg = request.values.get('Body')
   from_number = request.values.get('From')
-  u = get_or_create_user(from_number)
+  u = get_or_create_user(from_number, msg)
 
   # add element
   if valid_message(msg):
@@ -56,12 +58,22 @@ def sms():
 # Models
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
+  username_url = db.Column(db.String(80), unique=True)
+  username = db.Column(db.String(80))
   phone_number = db.Column(db.String(20), unique=True)
   data = db.Column(db.Text())
   
-  def __init__(self, phone_number, data = None):
+  def __init__(self, phone_number, username, data = None):
     self.phone_number = phone_number
+    self.username = username
+    self.username_url = self.username_to_url(username)
     self.data = data
+
+  def username_to_url(self, username):
+    url = re.sub('[!@#$]', '', username)
+    url = url.strip()
+    url = url.replace(" ","-")
+    return url
 
   def save_json_as_text(self, json_data):
     text_data = json.dumps(json_data)
@@ -78,13 +90,62 @@ class User(db.Model):
     data[datum['date']] = {'mood' : datum['mood'], 'note' : datum['note']}
     self.save_json_as_text(data)
 
+  def to_dict(self):
+    user_dict = self.__dict__
+    try:
+      del user_dict['_sa_instance_state']
+    except KeyError:
+      pass
+    return user_dict
+
+  @classmethod
+  def users_to_json_string(cls):
+    user_dicts = []
+    users = cls.query.all()
+    for u in users:
+      user_dicts.append(u.to_dict())
+    return json.dumps(user_dicts)
+
+  @classmethod
+  def users_to_file(cls, filepath = None):
+    users_string = cls.users_to_json_string()
+    
+    if filepath is None:
+      timestamp = format_datetime(datetime.now(), 'YYYY-MM-DD-HH')
+      filepath = 'data/%s-data.txt' % timestamp
+    
+    f = open(filepath, 'w')
+    f.write(users_string)
+    f.close()
+    return filepath
+
+  @classmethod
+  def load_users_from_file(cls, filepath):
+    users_string=open(filepath).read()
+    users = json.loads(users_string)
+    
+    new_users = []
+    error_count = 0
+    for u in users:
+      try:
+        new_u = User(phone_number = u['phone_number'],
+                    username = u['username'],
+                    data = u.get('data'))
+        db.session.add(new_u)
+        new_users.append(new_u)
+      except Exception:
+        error_count += 1
+    
+    db.session.commit()
+    return new_users, error_count
+
 # Utils
-def get_or_create_user(phone_number):
+def get_or_create_user(phone_number, username = None):
   u = User.query.filter_by(phone_number = phone_number).first()
   if u:
     return u
   else:
-    u = User(phone_number = phone_number)
+    u = User(phone_number = phone_number, username = username)
     db.session.add(u)
     welcome = "Welcome! Reply with 1 (terrible) to 5 (awesome) + a note about your day. You can see your mood map at http://mood-sms.herokuapp.com/%s" % u.phone_number
     send_message(u.phone_number, welcome)
